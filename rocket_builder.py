744488b8f64ce1cc4e2dfa6f7ac4755588a6d73d
Heiki Riesenkampf <heiki@mirage.id>
2019-05-10 16:49:30 +0200

    Fix linting issues and extract constants to config file

--- rocket_builder.py
+++ rocket_builder.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import os
 import cv2
 import torch
@@ -6,7 +7,7 @@ import json
 import numpy as np
 import torch.nn as nn
 from PIL import Image
-from PIL import ImageDraw
+# from PIL import ImageDraw
 from torchvision import transforms
 from .model import PoseEstimationWithMobileNet
 from .utils import normalize, pad_width, load_state, extract_keypoints, group_keypoints, BODY_PARTS_KPT_IDS, BODY_PARTS_PAF_IDS
@@ -35,6 +36,7 @@ def build(config_path: str = '') -> nn.Module:
     
     model.postprocess = types.MethodType(postprocess, model)
     model.preprocess = types.MethodType(preprocess, model)
+    setattr(model, 'config', config)
 
     return model
 
@@ -65,16 +67,16 @@ def preprocess(self, img: Image) -> torch.Tensor:
     else:
         raise TypeError("wrong input type: got {} but expected list of PIL.Image, "
                         "single PIL.Image or torch.Tensor".format(type(img)))
-    pad_value=(0, 0, 0)
-    img_mean=(128, 128, 128)
-    img_scale=1/256
-    net_input_height_size = 256
-    stride = 8
+    stride = self.config['stride']
+    pad_value = tuple(self.config['pad_value'])
+    img_mean = tuple(self.config['img_mean'])
+    img_scale = 1.0 / self.config['input_size'][0]
+    net_input_height_size = self.config['input_size'][0]
+
     np_img = np.array(img)
     img = cv2.cvtColor(np_img, cv2.COLOR_RGB2BGR)
     height, width, _ = img.shape
     scale = net_input_height_size / height
-
     scaled_img = cv2.resize(img, (0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
     scaled_img = normalize(scaled_img, img_mean, img_scale)
     min_dims = [net_input_height_size, max(scaled_img.shape[1], net_input_height_size)]
@@ -97,13 +99,14 @@ def postprocess(self, pose_output: torch.Tensor, input_img: Image, visualize: bo
         input_img (PIL.Image): Original input image which has not been preprocessed yet
         visualize (bool): If True outputs image with annotations else a list of bounding boxes
     """
-    net_input_height_size = 256
-    pad_value=(0, 0, 0)
-    img_mean=(128, 128, 128)
-    img_scale=1/256
-    stride = 8
-    upsample_ratio = 4
-    color = [0, 224, 255]
+    color = self.config['color']
+    stride = self.config['stride']
+    upsample_ratio = self.config['upsample_ratio']
+    pad_value = tuple(self.config['pad_value'])
+    img_mean = tuple(self.config['img_mean'])
+    img_scale = 1.0 / self.config['input_size'][0]
+    net_input_height_size = self.config['input_size'][0]
+
     np_img = np.array(input_img)
     img = cv2.cvtColor(np_img, cv2.COLOR_RGB2BGR)
     height, width, _ = img.shape
@@ -122,33 +125,33 @@ def postprocess(self, pose_output: torch.Tensor, input_img: Image, visualize: bo
     pafs = cv2.resize(pafs, (0, 0), fx=upsample_ratio, fy=upsample_ratio, interpolation=cv2.INTER_CUBIC)
     
     if visualize:
-      orig_img = img.copy()
-      total_keypoints_num = 0
-      all_keypoints_by_type = []
-      for kpt_idx in range(18):  # 19th for bg
-          total_keypoints_num += extract_keypoints(heatmaps[:, :, kpt_idx], all_keypoints_by_type, total_keypoints_num)
+        orig_img = img.copy()
+        total_keypoints_num = 0
+        all_keypoints_by_type = []
+        for kpt_idx in range(18):  # 19th for bg
+            total_keypoints_num += extract_keypoints(heatmaps[:, :, kpt_idx], all_keypoints_by_type, total_keypoints_num)
 
-      pose_entries, all_keypoints = group_keypoints(all_keypoints_by_type, pafs, demo=True)
-      for kpt_id in range(all_keypoints.shape[0]):
-          all_keypoints[kpt_id, 0] = (all_keypoints[kpt_id, 0] * stride / upsample_ratio - pad[1]) / scale
-          all_keypoints[kpt_id, 1] = (all_keypoints[kpt_id, 1] * stride / upsample_ratio - pad[0]) / scale
-      for n in range(len(pose_entries)):
-          if len(pose_entries[n]) == 0:
-              continue
-          for part_id in range(len(BODY_PARTS_PAF_IDS) - 2):
-              kpt_a_id = BODY_PARTS_KPT_IDS[part_id][0]
-              global_kpt_a_id = pose_entries[n][kpt_a_id]
-              if global_kpt_a_id != -1:
-                  x_a, y_a = all_keypoints[int(global_kpt_a_id), 0:2]
-                  cv2.circle(img, (int(x_a), int(y_a)), 3, color, -1)
-              kpt_b_id = BODY_PARTS_KPT_IDS[part_id][1]
-              global_kpt_b_id = pose_entries[n][kpt_b_id]
-              if global_kpt_b_id != -1:
-                  x_b, y_b = all_keypoints[int(global_kpt_b_id), 0:2]
-                  cv2.circle(img, (int(x_b), int(y_b)), 3, color, -1)
-              if global_kpt_a_id != -1 and global_kpt_b_id != -1:
-                  cv2.line(img, (int(x_a), int(y_a)), (int(x_b), int(y_b)), color, 2)
-      img = cv2.addWeighted(orig_img, 0.6, img, 0.4, 0)
-      cv2_im = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-      return Image.fromarray(cv2_im)
+        pose_entries, all_keypoints = group_keypoints(all_keypoints_by_type, pafs, demo=True)
+        for kpt_id in range(all_keypoints.shape[0]):
+            all_keypoints[kpt_id, 0] = (all_keypoints[kpt_id, 0] * stride / upsample_ratio - pad[1]) / scale
+            all_keypoints[kpt_id, 1] = (all_keypoints[kpt_id, 1] * stride / upsample_ratio - pad[0]) / scale
+        for n in range(len(pose_entries)):
+            if len(pose_entries[n]) == 0:
+                continue
+            for part_id in range(len(BODY_PARTS_PAF_IDS) - 2):
+                kpt_a_id = BODY_PARTS_KPT_IDS[part_id][0]
+                global_kpt_a_id = pose_entries[n][kpt_a_id]
+                if global_kpt_a_id != -1:
+                    x_a, y_a = all_keypoints[int(global_kpt_a_id), 0:2]
+                    cv2.circle(img, (int(x_a), int(y_a)), 3, color, -1)
+                kpt_b_id = BODY_PARTS_KPT_IDS[part_id][1]
+                global_kpt_b_id = pose_entries[n][kpt_b_id]
+                if global_kpt_b_id != -1:
+                    x_b, y_b = all_keypoints[int(global_kpt_b_id), 0:2]
+                    cv2.circle(img, (int(x_b), int(y_b)), 3, color, -1)
+                if global_kpt_a_id != -1 and global_kpt_b_id != -1:
+                    cv2.line(img, (int(x_a), int(y_a)), (int(x_b), int(y_b)), color, 2)
+        img = cv2.addWeighted(orig_img, 0.6, img, 0.4, 0)
+        cv2_im = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        return Image.fromarray(cv2_im)
     return pafs
